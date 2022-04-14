3d1e0b02a2262273820ca898b6d41faeff4a6d2c
Amit Choudhary <amitchoudhary1817@gmail.com>
2022-04-14 08:22:19 +0530

    Sending product and user queries

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const expressValidator =  require('express-validator');
 require('dotenv').config();  // This will allow us to use env varibles in .env file
 
@@ -24,6 +25,7 @@ app.use(morgan('dev'));  // dev --> flag
 app.use(bodyParser.json());  // .json --> so that we can get json data from request.body
 app.use(cookieParser());
 app.use(expressValidator());
+app.use(cors());
 
 // routes middleware
 app.use('/api',authRoutes);
--- controllers/product.js
+++ controllers/product.js
@@ -4,9 +4,9 @@ const _ = require('lodash');
 const fs = require('fs');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
-exports.productById = (req,res,next,id) => {
-    Product.findById(id).exec((err,product) => {
-        if(err || !product){
+exports.productById = (req, res, next, id) => {
+    Product.findById(id).exec((err, product) => {
+        if (err || !product) {
             return res.status(400).json({
                 error: "Product not found"
             });
@@ -16,7 +16,7 @@ exports.productById = (req,res,next,id) => {
     });
 };
 
-exports.read = (req,res) => {
+exports.read = (req, res) => {
     req.product.photo = undefined;
     return res.json(req.product);
 }
@@ -35,7 +35,7 @@ exports.create = (req, res) => {
 
         // check for all fields 
         const { name, description, price, category, quantity, shipping } = fields;
-        if(!name || !description || !price || !category || !quantity || !shipping){
+        if (!name || !description || !price || !category || !quantity || !shipping) {
             return res.status(400).json({
                 error: 'All fields are required'
             });
@@ -72,10 +72,10 @@ exports.create = (req, res) => {
     })
 }
 
-exports.remove = (req,res) => {
+exports.remove = (req, res) => {
     let product = req.product;
     product.remove((err, deletedProduct) => {
-        if(err){
+        if (err) {
             return res.status(400).json({
                 error: errorHandler(err)
             })
@@ -100,7 +100,7 @@ exports.update = (req, res) => {
 
         // check for all fields 
         const { name, description, price, category, quantity, shipping } = fields;
-        if(!name || !description || !price || !category || !quantity || !shipping){
+        if (!name || !description || !price || !category || !quantity || !shipping) {
             return res.status(400).json({
                 error: 'All fields are required'
             });
@@ -137,3 +137,123 @@ exports.update = (req, res) => {
 
     })
 }
+
+/*
+*   sell / arrival
+*   by sell = /products?sortBy=sold&order=desc&limit=4 
+*   by arrival = /products?sortBy=createdAt&order=desc&limit=4 
+*   if no params are sent, then all products are returned
+*/
+
+exports.list = (req, res) => {
+    let order = req.query.order ? req.query.order : 'asc';
+    let sortBy = req.query.sortBy ? req.query.sortBy : '_id';
+    let limit = req.query.limit ? parseInt(req.query.limit) : 6;
+
+    console.log(order);
+    Product.find()
+        .select('-photo')
+        .populate('category')
+        .sort([[sortBy, order]])
+        .limit(limit)
+        .exec((err, products) => {
+            if (err) {
+                return res.status(400).json({
+                    error: 'Products not found'
+                })
+            }
+            res.json(products);
+        })
+};
+
+/*
+*   it will find the products based on the req product category
+*   other products that has the same category, will be returned
+*/
+
+exports.listRelated = (req, res) => {
+    let limit = req.query.limit ? parseInt(req.query.limit) : 6;
+
+    Product.find({ _id: { $ne: req.product } , category: req.product.category })  // ne --> not included provided by mongoose/mongodb
+        .limit(limit)
+        .populate('category', '_id name')
+        .exec((err, products) => {
+            if (err) {
+                return res.status(400).json({
+                    error: 'Products not found'
+                })
+            }
+            res.json(products);
+        })
+}
+
+exports.listCategories = (req,res) => {
+    Product.distinct('category', {}, (err, categories) => { // in middle object we can pass queries
+        if (err) {
+            return res.status(400).json({
+                error: 'Category not found'
+            })
+        }
+        res.json(categories);
+    })  
+}
+
+/**
+ * list products by search
+ * we will implement product search in react frontend
+ * we will show categories in checkbox and price range in radio buttons
+ * as the user clicks on those checkbox and radio buttons
+ * we will make api request and show the products to users based on what he wants
+ */
+
+exports.listBySearch = (req, res) => {
+    let order = req.body.order ? req.body.order : "desc";
+    let sortBy = req.body.sortBy ? req.body.sortBy : "_id";
+    let limit = req.body.limit ? parseInt(req.body.limit) : 100;
+    let skip = parseInt(req.body.skip);
+    let findArgs = {};
+
+    // console.log(order, sortBy, limit, skip, req.body.filters);
+    // console.log("findArgs", findArgs);
+
+    for (let key in req.body.filters) {
+        if (req.body.filters[key].length > 0) {
+            if (key === "price") {
+                // gte -  greater than price [0-10]
+                // lte - less than
+                findArgs[key] = {
+                    $gte: req.body.filters[key][0],  // $gte, $lte are provided by mongoose/mongodb
+                    $lte: req.body.filters[key][1]
+                };
+            } else {
+                findArgs[key] = req.body.filters[key];
+            }
+        }
+    }
+
+    Product.find(findArgs)
+        .select("-photo")
+        .populate("category")
+        .sort([[sortBy, order]])
+        .skip(skip)
+        .limit(limit)
+        .exec((err, data) => {
+            if (err) {
+                return res.status(400).json({
+                    error: "Products not found"
+                });
+            }
+            res.json({
+                size: data.length,
+                data
+            });
+        });
+};
+
+exports.photo = (req,res,next) => {
+    if(req.product.photo.data){
+        res.set('Content-Type', req.product.photo.contentType);
+        return res.send(req.product.photo.data);
+    }
+    next();
+};
